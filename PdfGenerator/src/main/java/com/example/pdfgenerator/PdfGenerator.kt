09c33ecaa4a119666771f680db66cdd4fda724bc
Neeraj Raut <neeraj.raut@coditas.com>
2022-11-03 09:39:10 +0530

    changed library type from class to object

--- PdfGenerator/src/main/java/com/example/pdfgenerator/PdfGenerator.kt
+++ PdfGenerator/src/main/java/com/example/pdfgenerator/PdfGenerator.kt
@@ -8,8 +8,8 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
-class PdfGenerator {
-    public fun createPDF(
+object PdfGenerator {
+    fun createPDF(
         context: Context,
         profession: String,
         summary: String,
