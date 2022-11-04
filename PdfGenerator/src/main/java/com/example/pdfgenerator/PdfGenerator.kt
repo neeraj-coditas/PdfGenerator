5ba3d630f72cad5641ab4b31e3d5524e70209d0d
Neeraj Raut <neeraj.raut@coditas.com>
2022-11-04 16:13:19 +0530

    canvas update

--- PdfGenerator/src/main/java/com/example/pdfgenerator/PdfGenerator.kt
+++ PdfGenerator/src/main/java/com/example/pdfgenerator/PdfGenerator.kt
@@ -23,30 +23,18 @@ object PdfGenerator {
         val myPdfDocument = PdfDocument()
         val myPaint = Paint()
 
-        val mypageInfo1 = PdfDocument.PageInfo.Builder(400, 600, 1).create()
-        val mypage1 = myPdfDocument.startPage(mypageInfo1)
-        val canvas = mypage1.canvas
-        canvas.drawText("Welcome to Page 1", 40F, 50F, myPaint)
-        myPdfDocument.finishPage(mypage1)
-
-        val mypageInfo2 = PdfDocument.PageInfo.Builder(400, 600, 2).create()
-        val mypage2 = myPdfDocument.startPage(mypageInfo2)
-        val canvas2 = mypage2.canvas
-        canvas2.drawText("Welcome to Page 2", 40F, 50F, myPaint)
-        myPdfDocument.finishPage(mypage2)
-
-        val mypageInfo3 = PdfDocument.PageInfo.Builder(400, 600, 3).create()
-        val mypage3 = myPdfDocument.startPage(mypageInfo3)
-        val canvas3 = mypage3.canvas
-        canvas3.drawText(profession, 40F, 50F, myPaint)
-        canvas3.drawText(summary, 40F, 70F, myPaint)
-        canvas3.drawText(designation, 40F, 90F, myPaint)
-        canvas3.drawText(experience, 40F, 110F, myPaint)
-        canvas3.drawText(primarySkill, 40F, 130F, myPaint)
-        canvas3.drawText(secondarySkill, 40F, 150F, myPaint)
-        canvas3.drawText(primaryDomain, 40F, 170F, myPaint)
-        canvas3.drawText(secondaryDomain, 40F, 190F, myPaint)
-        myPdfDocument.finishPage(mypage3)
+        val mypageInfo = PdfDocument.PageInfo.Builder(400, 600, 3).create()
+        val mypage = myPdfDocument.startPage(mypageInfo)
+        val canvas = mypage.canvas
+        canvas.drawText(profession, 40F, 50F, myPaint)
+        canvas.drawText(summary, 40F, 70F, myPaint)
+        canvas.drawText(designation, 40F, 90F, myPaint)
+        canvas.drawText(experience, 40F, 110F, myPaint)
+        canvas.drawText(primarySkill, 40F, 130F, myPaint)
+        canvas.drawText(secondarySkill, 40F, 150F, myPaint)
+        canvas.drawText(primaryDomain, 40F, 170F, myPaint)
+        canvas.drawText(secondaryDomain, 40F, 190F, myPaint)
+        myPdfDocument.finishPage(mypage)
 
         val file = File(context.getExternalFilesDir("/"), "FirstPDF.pdf")
 
